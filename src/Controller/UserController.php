8e0f9ad57fe29ace1506753ab2ce1da38f47b380
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-11-23 22:14:02 +0100

    added create user function

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\Type\UserType;
+use DateTime;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,5 +19,40 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+
+    #[Route('/user/add', name: 'app_add_user')]
+    public function addUser(ManagerRegistry $doctrine, Request $request): Response
+    {
+
+        $user = new User();
+        $user->setCreationDate(new DateTime());
+        $user->setPoints(0);
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user->setName($form->getData()->getName());
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'user successfully created.');
+
+        }
+
+            
+
+        return $this->renderForm('user/add.html.twig', [
+            'form' => $form,
+        ]);
+
+
+        
+
+
+
     }
 }
